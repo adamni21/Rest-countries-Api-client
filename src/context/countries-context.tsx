daba0d2c0f39a5d8c8c0c1790d42596ef5dc0e28
adamni21 <85580454+adamni21@users.noreply.github.com>
2022-02-28 20:55:18 +0100

    search/filter-logic refactoring

--- src/context/countries-context.tsx
+++ src/context/countries-context.tsx
@@ -13,15 +13,14 @@ const countriesContextDefault: CountriesContextState = {
   setRegion: () => {},
 };
 
-const somePartialMatches = (name: string, searchValue: string) => {
-  if (
+const middleNameMatches = (name: string, searchValue: string) => {
+  return (
     name.includes(" ") &&
     name
       .split(" ")
       .slice(1)
-      .some((partial) => partial.startsWith(searchValue))
-  )
-    return true;
+      .some((middleName) => middleName.startsWith(searchValue))
+  );
 };
 
 const filterCountries = (
@@ -30,49 +29,51 @@ const filterCountries = (
   countries: Country[]
 ) => {
   if (!countries.length) return [];
-  const result: { country: Country; rating: number }[] = [];
+
+  const countriesInRegion =
+    region === Region.all
+      ? countries
+      : // startsWith() due to typo in API ("Americas" instead of "America")
+        countries.filter((country) => country.region.startsWith(region));
+
+  // "searchValue" is empty, skips search
+  if (!searchValue) return countriesInRegion;
+
+  const searchRatings: { country: Country; rating: number }[] = [];
   const lowerCaseSearchValue = searchValue.toLowerCase();
-  for (let i = 0; i < countries.length; i++) {
-    // skips country if region is set and does'nt match;  startsWith due to typo in API ("Americas" instead of "America")
-    if (region !== Region.all && !countries[i].region.startsWith(region))
-      continue;
-
-    // if country starts with searchValue
-    const name = countries[i].name.toLowerCase();
+  for (let i = 0; i < countriesInRegion.length; i++) {
+    // name of country starts with searchValue
+    const name = countriesInRegion[i].name.toLowerCase();
     if (name.startsWith(lowerCaseSearchValue)) {
-      result.push({ country: countries[i], rating: 1.3 });
+      searchRatings.push({ country: countriesInRegion[i], rating: 1.3 });
       continue;
     }
 
-    // if searchValue for example matches "States" in "United States"
-    if (somePartialMatches(name, lowerCaseSearchValue)) {
-      result.push({ country: countries[i], rating: 1.2 });
+    // example: "States" in "United States" starts with "searchValue"
+    if (middleNameMatches(name, lowerCaseSearchValue)) {
+      searchRatings.push({ country: countriesInRegion[i], rating: 1.2 });
       continue;
     }
 
-    let nativeNamesPartialMatches = false;
-    const nativeNames = countries[i].nativeNames?.map((nativeName) => {
-      const lowerName = nativeName.toLowerCase();
-      // if native name partial matches
-      if (somePartialMatches(nativeName, lowerCaseSearchValue))
-        nativeNamesPartialMatches = true;
-      // return lower case name
-      return lowerName;
-    });
-
-    // if a native name starts with searchValue
+    // "nativeNames" to lower case
+    const nativeNames = countriesInRegion[i].nativeNames?.map((name) =>
+      name.toLowerCase()
+    );
+    // a native name starts with searchValue
     if (nativeNames?.some((name) => name.startsWith(lowerCaseSearchValue))) {
-      result.push({ country: countries[i], rating: 1.1 });
+      searchRatings.push({ country: countriesInRegion[i], rating: 1.1 });
       continue;
     }
 
-    if (nativeNamesPartialMatches) {
-      result.push({ country: countries[i], rating: 1 });
+    if (
+      nativeNames?.some((name) => middleNameMatches(name, lowerCaseSearchValue))
+    ) {
+      searchRatings.push({ country: countriesInRegion[i], rating: 1 });
       continue;
     }
 
     // default
-    // nGrams of country name(s). one char longer than the searchValue for the case that a char was missed
+    // nGrams of country name(s), grams are one char longer than the "searchValue" in case a char was missed
     const nGrams = [
       // adds name or nGrams of name if long enough
       ...(searchValue.length + 1 < name.length
@@ -92,12 +93,16 @@ const filterCountries = (
     const highestRating = Math.max(
       ...nGrams.map((gram) => diceCoefficient(gram, lowerCaseSearchValue))
     );
-    // pushes country to result if rating is greater than zero
+    // pushes country to searchRatings if rating is greater than zero
     if (highestRating > 0)
-      result.push({ country: countries[i], rating: highestRating });
+      searchRatings.push({
+        country: countriesInRegion[i],
+        rating: highestRating,
+      });
   }
-  // returns result sorted and mapped back to just countries
-  return result
+
+  // returns searchRatings sorted and mapped back to just countries
+  return searchRatings
     .sort((a, b) => b.rating - a.rating)
     .map((country) => country.country);
 };
@@ -109,9 +114,11 @@ const CountriesProvider: FC = ({ children }) => {
   const [filteredCountries, setFilteredCountries] = useState<Country[]>([]);
   const [searchValue, setSearchValue] = useState<string>("");
   const [region, setRegion] = useState<Region>(Region.all);
-  useEffect(()=>{
-    setFilteredCountries(filterCountries(searchValue, region, countries))
-  }, [countries, searchValue, region])
+  useEffect(() => {
+    console.time();
+    setFilteredCountries(filterCountries(searchValue, region, countries));
+    console.timeEnd();
+  }, [countries, searchValue, region]);
 
   return (
     <CountriesContext.Provider
